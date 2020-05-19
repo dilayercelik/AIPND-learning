e6c355e3cc349b989b6b4e28200364f434c67cb3
Dilay Ercelik <63108603+dilayercelik@users.noreply.github.com>
2020-05-20 00:32:38 +0200

    Add files via upload

---
+++ numpy/Quizzes/create_ndarray.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May 20 00:31:17 2020
+
+@author: dilayerc
+"""
+
+
+import numpy as np
+
+# Using the Built-in functions you learned about in the
+# previous lesson, create a 4 x 4 ndarray that only
+# contains consecutive even numbers from 2 to 32 (inclusive)
+
+X = np.arange(2, 33, 2).reshape((4,4))
